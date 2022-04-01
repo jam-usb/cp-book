93960ca302a1de3a337e232f76ebadad40589588
Angel <angelmolina19@yahoo.es>
2022-04-01 11:40:32 -0400

    DCHT

--- content/various/DCHT.h
+++ content/various/DCHT.h
@@ -2,34 +2,35 @@
  * Author: bomba de tenedores
  * Description: Contenedor que puede agregar lineas mx + b y preguntar max x
 */
-struct Line{
-    mutable ll m, b, p;
-    bool operator<(const Line& o) const { return m < o.m; }
-    bool operator<(ll x) const { return p < x; }
-};
 
-struct LineContainer : multiset<Line, less<>> {
-    const ll inf = LLONG_MAX;
-    // (para doubles usar inf = 1/.0, div(a, b) = a / b;)
+bool Q; // maximizar o minimizar
+struct Line {
+    mutable ll k, m, p;
+    bool operator<(const Line& o) const {
+        return Q ? p < o.p : k < o.k;
+    }
+};
+struct LineContainer : multiset<Line> {
+    const ll inf = INFLL;
     ll div(ll a, ll b){
         return a / b - ((a ^ b) < 0 && a % b);
     }
-    bool isect(iterator x, iterator y){
-        if(y == end()) { x->p = inf; return false; }
-        if(x->m == y->m) x->p = x->b > y->b ? inf : -inf;
-        else x->p = div(y->b - x->b, x->m - y->m);
+    bool isect(iterator x, iterator y) {
+        if (y == end()) { x->p = inf; return false; }
+        if (x->k == y->k) x->p = x->m > y->m ? inf : -inf;
+        else x->p = div(y->m - x->m, x->k - y->k);
         return x->p >= y->p;
     }
-    void add(ll m, ll b){
-        auto z = insert({m, b, 0}), y = z++, x = y;
-        while(isect(y, z)) z = erase(z);
-        if(x != begin() && isect(--x, y)) isect(x, y = erase(y));
-        while((y = x) != begin() && (--x)->p >= y->p)
+    void add(ll k, ll m) { // <pendiente,altura>
+        auto z = insert({k, m, 0}), y = z++, x = y;
+        while (isect(y, z)) z = erase(z);
+        if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
+        while ((y = x) != begin() && (--x)->p >= y->p)
             isect(x, erase(y));
     }
-    ll query(ll x){
-        assert(!empty());
-        auto l = *lower_bound(x);
-        return l.m * x + l.b;
+    ll query(ll x) {
+        //assert(!empty());
+        Q = 1; auto l = *lower_bound({0,0,x}); Q = 0;
+        return l.k * x + l.m;
     }
 };
