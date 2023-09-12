9c29f790c7ddf7434cf622bc145b9d9b54a26e47
Jesus Wahrman <jesuswahrman@gmail.com>
2023-09-12 13:54:56 +0200

    hk

--- content/graph/Hopcroft-Karp.h
+++ content/graph/Hopcroft-Karp.h
@@ -10,6 +10,10 @@ Problemas de mathing
 * - Conjunto de aristas minimo que cubra los 2 conjundores del Grafo: MatchingMaximo y se completan lso nodos que quedaron solos
 * - Minimo cubrimiento por nodos: Se busca corte minimo y los extremos  de los edges cortados son los nodos
 * - Maximo conjunto independiente (conjunto de vertices que no se  conectan): Complemento de los nodos del problema 3 o lo que es lo  mismo #nodos - maxMatching
+* - Como encontrar nodos del Minimum Vertex Cover (los del Maximum Independent Set son el complemento):
+* - Give orientation to the edges, matched edges start from the right side of the graph to the left side, and free edges start from the left side of the graph to the right side.
+* - Run DFS from unmatched nodes of the left side, in this traversal some nodes will become visited, others will stay unvisited.
+* - The MVC nodes are the visited nodes from the right side, and unvisited nodes from the left side.
 */
 // A from 1 to A
 // B from A+1 to A+B < MAXN
@@ -27,7 +31,7 @@ bool bfs() {
 	while(!q.empty()) {
 		int v = q.front(); q.pop();
 		if(v!=0) {
-			FOR(i,0,G[v].size()){
+			FOR(i,0,sz(G[v])){
 				int u = G[v][i];
 				if(dist[match[u]]==INF) {
 					dist[match[u]] = dist[v] + 1;
@@ -40,7 +44,7 @@ bool bfs() {
 }
 bool dfs(int v) {
 	if(v!=0) {
-		FOR(i,0,G[v].size()){
+		FOR(i,0,sz(G[v])){
 			int u = G[v][i];
 			if(dist[match[u]]==dist[v]+1) {
 				if(dfs(match[u])) {
