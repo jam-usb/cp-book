348da94ce660025b25629b1dd06a769a42192517
Jesus Wahrman <jesuswahrman@gmail.com>
2023-09-12 13:57:14 +0200

    noc

--- content/graph/Hopcroft-Karp.h
+++ content/graph/Hopcroft-Karp.h
@@ -10,7 +10,7 @@ Problemas de mathing
 * - Conjunto de aristas minimo que cubra los 2 conjundores del Grafo: MatchingMaximo y se completan lso nodos que quedaron solos
 * - Minimo cubrimiento por nodos: Se busca corte minimo y los extremos  de los edges cortados son los nodos
 * - Maximo conjunto independiente (conjunto de vertices que no se  conectan): Complemento de los nodos del problema 3 o lo que es lo  mismo #nodos - maxMatching
-* - Como encontrar nodos del Minimum Vertex Cover (los del Maximum Independent Set son el complemento):
+* Como encontrar nodos del Minimum Vertex Cover (los del Maximum Independent Set son el complemento):
 * - Give orientation to the edges, matched edges start from the right side of the graph to the left side, and free edges start from the left side of the graph to the right side.
 * - Run DFS from unmatched nodes of the left side, in this traversal some nodes will become visited, others will stay unvisited.
 * - The MVC nodes are the visited nodes from the right side, and unvisited nodes from the left side.
